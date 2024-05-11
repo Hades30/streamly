5f21f50a4713b6c39a95420eb5d97cbba54bf4b5
Hades30 <tanishkumar30@gmail.com>
2024-05-11 14:34:49 +0530

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
       <div className="flex flex-col m-auto">
         <Image src="/stream.png" width={100} height={100} className="m-auto" />
         <span style={{ marginTop: "8px" }}>
-          Watch movies, shows free on Streamly!
+          Watch movies and shows free on{" "}
+          <span className="font-bold">Streamly!</span>
         </span>
       </div>
     </main>
